d2d1c5ddc9a21c9fbb7fbae9fcd326988bd25c5e
KAPIL SINGHA ETHUN <s2211078145@ru.ac.bd>
2025-02-19 18:34:49 +0600

    Update script.sh

--- evolutionX/script.sh
+++ evolutionX/script.sh
@@ -18,7 +18,7 @@ repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 
 # cloning device tree
 git clone https://github.com/daisy-mi-a2-lite/device_xiaomi_daisy.git device/xiaomi/daisy
-git clone https://github.comxiaomi-msm8953-devs/android_device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common
+git clone https://github.com/xiaomi-msm8953-devs/android_device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common
 
 # cloning kernel tree
 git clone https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953.git kernel/xiaomi/msm8953
